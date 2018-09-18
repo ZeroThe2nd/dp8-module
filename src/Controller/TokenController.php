5d8bfeb00198f7be0b6339b01bab1f13aac0f9c1
Pepijn Zegveld <pepijn@ezcompany.nl>
2018-09-18 14:59:17 +0200

    Fixes for the Token controller

--- src/Controller/TokenController.php
+++ src/Controller/TokenController.php
@@ -15,15 +15,30 @@ class TokenController {
     $this->tokenService = new TokenService;
   }
 
-  public function generate($length = 64) {
+  /**
+   * Ask service to generate a token with the given length for us.
+   * Defaults to the config if no value is passed.
+   * Updates the token charset if set in the config
+   *
+   * @param int|null $length
+   *
+   * @return array
+   */
+  public function generate($length = NULL) {
     $config = \Drupal::config('poc.settings');
-    $charset = $config->get('poc.TokenAlphabet');
-    if (!is_null($charset)) {
-      $this->tokenService->setTokenChars((string) $charset);
+    $tokenCharset = (string) $config->get('poc.TokenCharset');
+    $tokenLength = (int) $config->get('poc.TokenLength');
+
+    if (is_null($length) && empty($tokenLength)) {
+      $tokenCharset = 64;
+    }
+
+    if (!is_null($tokenCharset)) {
+      $this->tokenService->setTokenChars($tokenCharset);
     }
 
     return [
-      '#token' => $this->tokenService->generateToken($length),
+      '#token' => $this->tokenService->generateToken($tokenLength),
       '#theme' => 'token',
       '#attached' => [
         'library' => [
