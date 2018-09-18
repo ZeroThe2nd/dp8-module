accf5e807516927d2e4bc7218ddf05ecf0274de6
Pepijn Zegveld <pepijn@ezcompany.nl>
2018-09-18 13:20:02 +0200

    App style updates

--- js/App.js
+++ js/App.js
@@ -72,8 +72,9 @@ document.addEventListener('DOMContentLoaded', () => {
     el: '#kaomojiApp',
     template: '<div class="card kaomojiCard" @click="newKaomoji">'
       + '<div class="body">'
-      + '<div class="kaomoji" id="kaomoji">{{ kaomoji }}</div>'
-      + '<p><small>Click for a new Kaomoji</small></p>'
+      + '<div class="kaomoji">{{ kaomoji }}</div>'
+      + '</div><div class="actions">'
+      + '<button class="material-button small">Click for a new Kaomoji</button>'
       + '</div></div>',
     data: () => ({
       kaomoji: 'LOADING'
